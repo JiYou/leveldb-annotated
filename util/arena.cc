3f06ba2c6efb852a758d53d72949d3b9a59f3e16
JiYou <jiyou09@gmail.com>
2018-10-02 09:25:53 +0800

    阅读Arena内存管理
    Signed-off-by: JiYou <jiyou09@gmail.com>

--- util/arena.cc
+++ util/arena.cc
@@ -45,11 +45,25 @@ char* Arena::AllocateAligned(size_t bytes) {
   const int align = (sizeof(void*) > 8) ? sizeof(void*) : 8;
   // 确保是2的指数次方
   assert((align & (align-1)) == 0);   // Pointer size should be a power of 2
-  // 
+  // 取得当前地址未对齐的尾数
+  // 比如，要对齐的要求是8bytes
+  // 但是当前指针指向的是0x017这里。
+  // 那么余下的current_mod就是1
   size_t current_mod = reinterpret_cast<uintptr_t>(alloc_ptr_) & (align-1);
+  // 如果当前地址是0x17, 要求对齐是8 bytes
+  // 那么current_mod = 1, slop 就是7
   size_t slop = (current_mod == 0 ? 0 : align - current_mod);
+  // bytes + slop，
+  // 就是把余下的这个slop = 7算在新的申请者头上
+  // 返回的时候，直接向前移动slop个bytes
+  // 就完成了对齐。
   size_t needed = bytes + slop;
+  // 总结一下：AllocateAligned就是需要计算一下
+  // 对齐地址
+
   char* result;
+  // 这里的逻辑就与Allocate完全一样的了。
+  // 除了会移动一下slop以外。
   if (needed <= alloc_bytes_remaining_) {
     result = alloc_ptr_ + slop;
     alloc_ptr_ += needed;
@@ -58,6 +72,7 @@ char* Arena::AllocateAligned(size_t bytes) {
     // AllocateFallback always returned aligned memory
     result = AllocateFallback(bytes);
   }
+  // 这里断言一下，返回地址result肯定是对齐的。
   assert((reinterpret_cast<uintptr_t>(result) & (align-1)) == 0);
   return result;
 }
--- util/arena.h
+++ util/arena.h
@@ -28,6 +28,12 @@ class Arena {
 
   // Returns an estimate of the total memory usage of data allocated
   // by the arena.
+  // 统计所有由Arena生成的内存总数
+  // 这里面可能包含一些内存碎片
+  // 所以返回的是近似值
+  // 其实没有必要把memory_usage_设置成AtomicPointer的
+  // 直接设置成一般变量就可以了
+  // 比如size_t memory_usage_;
   size_t MemoryUsage() const {
     return reinterpret_cast<uintptr_t>(memory_usage_.NoBarrier_Load());
   }
@@ -55,13 +61,22 @@ inline char* Arena::Allocate(size_t bytes) {
   // The semantics of what to return are a bit messy if we allow
   // 0-byte allocations, so we disallow them here (we don't need
   // them for our internal use).
+  // 说这么多，实际上就是不允许只申请bytes > 0的情况
   assert(bytes > 0);
+  // 如果当前块余下的空间还够用
   if (bytes <= alloc_bytes_remaining_) {
+    // 得到当前块的指针头
     char* result = alloc_ptr_;
+    // 移动指针
     alloc_ptr_ += bytes;
+    // 更新余下的bytes数
     alloc_bytes_remaining_ -= bytes;
     return result;
   }
+  // 当余下的空间不够用的时候，这里就去申请一个新块
+  // 如果要的bytes数目是大于1k，那么就申请bytes那么多。
+  // 如果要的bytes数目小于1k，那么新申请的时候，就
+  // 按照4k来申请，并且从里面扣.
   return AllocateFallback(bytes);
 }
 
